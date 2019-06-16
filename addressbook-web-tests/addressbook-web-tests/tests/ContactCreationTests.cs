cd6ac20472fc163518ea4b09eb60586a9226fe9a
marina.grigoreva <marina.grigoreva@IPRW015>
2019-06-16 15:50:43 +0400

    2.4

---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace WebAddressbookTests
+
+{
+    [TestFixture]
+    public class ContactCreationTests : TestBase
+
+    {
+
+
+        [Test]
+        public void ContactCreationTest()
+        {            
+            ContactData contact = new ContactData("New");
+            contact.Lastname = "Contact";
+//            contact.Photo = "C:\\img\\1.png";
+            app.Contacts.Create(contact);
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactModificationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests 
+{
+    public class ContactModificationTests : TestBase
+    {
+
+        [Test]
+        public void ContactCreationTest()
+        {
+
+            ContactData newData = new ContactData("Test");
+            newData.Lastname = "Test";
+//            newData.Photo = "C:\\img\\1.png";
+            app.Contacts.Modify(1, newData);
+
+
+
+
+            
+            
+        }
+    }
+}
